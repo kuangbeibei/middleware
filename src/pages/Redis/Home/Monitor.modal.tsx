d7593b51ee7b7c38d9bc172718d5ddff45d9d6a9
kup <332518418@qq.com>
2019-11-22 15:28:04 +0800

    monitor init

--- src/pages/Redis/Home/Monitor.modal.tsx
+++ src/pages/Redis/Home/Monitor.modal.tsx
@@ -5,17 +5,67 @@
  */ 
 
 import * as React from "react"
-import Modal from "@com/Modal";
+import { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import setDrawerVisibility from "@actions/setDrawerVisibility";
 
-export default function (props) {
-    return <Modal
-			modalName={`Redis监控状态`}
-			visible={false}
-			handleOk={() => {}}
-			handleCancel={() => {}}
-    >
-        监控状态
-        </Modal>
+import { Drawer, Descriptions } from "antd";
+
+function MonitorDrawer(props) {
+
+    const { drawerVisibility, setDrawerVisibility, data } = props;
+
+	useEffect(() => {
+		setDrawerVisibility();
+	}, []);
+
+	const handleClose = () => {
+		setDrawerVisibility();
+    };
+    
+    return (
+		<Drawer
+			title="redis集群配置信息"
+			placement="right"
+			width="700px"
+			closable={false}
+			onClose={handleClose}
+			visible={drawerVisibility.visible}
+		>
+			<Descriptions bordered column={1}>
+				{data &&
+					Array.isArray(data) &&
+					data.map(configItem => {
+						let val = configItem.value.replace(/\n/g, "\n");
+						return (
+							<Descriptions.Item
+								key={configItem.enName}
+								label={configItem.name}
+							>
+								{configItem.enName === "moreConf" ||
+								configItem.enName === "instances" ||
+								configItem.enName === "defaultConf" ? (
+									<pre>{val}</pre>
+								) : (
+									<span>{configItem.value || "无"}</span>
+								)}
+							</Descriptions.Item>
+						);
+					})}
+			</Descriptions>
+		</Drawer>
+	);
 }
+
+export default connect(
+	(state: any) => ({
+		drawerVisibility: state.drawerVisibility
+	}),
+	dispatch => ({
+		setDrawerVisibility: bindActionCreators(setDrawerVisibility, dispatch)
+	})
+)(MonitorDrawer);
+
 
  
