d9b6e4f8fea93c96c84abccea775236eb8edf089
kup <332518418@qq.com>
2019-12-02 19:00:56 +0800

    webpack

--- build/webpack.build.conf.js
+++ build/webpack.build.conf.js
@@ -1,4 +1,39 @@
-
 module.exports = {
-    mode: 'production'
-}
+    mode: "production",
+    optimization: {
+        minimize: true,
+        namedModules: true,
+        namedChunks: true,
+        splitChunks: {
+            chunks: "all",
+            minSize: 30000,
+            maxSize: 0,
+            minChunks: 1,
+            maxAsyncRequests: 6,
+            maxInitialRequests: 4,
+            automaticNameDelimiter: "~",
+            automaticNameMaxLength: 30,
+            cacheGroups: {
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    priority: -10,
+                    name: 'middleware.vendor.js',
+                    enforce: true
+                },
+                commons: {
+                    name: 'commons',
+                    chunks: 'initial',
+                    minChunks: 2
+                },
+                default: {
+                    minChunks: 2,
+                    priority: -20,
+                    reuseExistingChunk: true
+                }
+            },
+        },
+        runtimeChunk: {
+            name: 'middleware.runtime.js'
+        }
+    }
+};
