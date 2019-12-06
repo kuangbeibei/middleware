ce565f3d22d3cd54f510958ef797d1b5fd6b33ba
kup <332518418@qq.com>
2019-12-06 10:17:11 +0800

    alarm api

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -2,7 +2,7 @@ export const MiddlewarePathPrefix = 'middleware'
 
 const ProductMidApiUrl = '/api-mid-deploy-redis.api.fz.yonghui.cn';
 const ProductUumApiUrl = '/api-uum.api.fz.yonghui.cn';
-const ProductAlarmApiUrl = '/mwp.api.fz.yonghui.cn'
+const ProductAlarmApiUrl = '/api-mwp.api.fz.yonghui.cn';
 
 export const ProductApiUrl = {
     ProductMidApiUrl,
