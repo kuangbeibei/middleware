334726edc1f6f96e08bb8ed38e2ea7731d17e448
kup <332518418@qq.com>
2019-12-24 16:11:38 +0800

    datepicker

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -42,3 +42,43 @@ export const clusterStatus = {
 		text: '初始状态',
 	}
 }
+
+export const dateLocale =  {
+	lang: {
+		placeholder: "选择日期",
+		rangePlaceholder: ["开始时间", "结束时间"],
+		today: "今天",
+		now: "现在",
+		backToToday: "回到今天",
+		ok: "确定",
+		clear: "清空",
+		month: "月",
+		year: "年",
+		timeSelect: "选择时间",
+		dateSelect: "选择日期",
+		monthSelect: "选择月份",
+		yearSelect: "选择年份",
+		decadeSelect: "选择十年",
+		yearFormat: "YYYY",
+		dateFormat: "M/D/YYYY",
+		dayFormat: "D",
+		dateTimeFormat: "M/D/YYYY HH:mm:ss",
+		monthFormat: "MMMM",
+		monthBeforeYear: true,
+		previousMonth: "上一个月",
+		nextMonth: "下一个月",
+		previousYear: "上一年",
+		nextYear: "下一年",
+		previousDecade: "上一个十年",
+		nextDecade: "下一个十年",
+		previousCentury: "上一个世纪",
+		nextCentury: "下一个世纪"
+	},
+	timePickerLocale: {
+		placeholder: "选择时间"
+	},
+	dateFormat: "YYYY-MM-DD",
+	dateTimeFormat: "YYYY-MM-DD HH:mm:ss",
+	weekFormat: "YYYY-wo",
+	monthFormat: "YYYY-MM"
+};
