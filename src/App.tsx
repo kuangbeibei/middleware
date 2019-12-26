b82f5f40668939aa7e7387f71e6cb30a9a937a64
kup <332518418@qq.com>
2019-12-26 18:07:48 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { useEffect } from 'react'
 import {
 	BrowserRouter as Router,
 	Route,
@@ -9,29 +8,17 @@ import {
 
 import PageIndex from "@pages/Index";
 import Nomatch from "@pages/No-match";
-import { postApi } from "@api";
 
-declare var NODE_ENV: string;
 const App: React.SFC = props => {
-
-  useEffect(()=> {
-    // 在本地开发，模拟一次登录操作
-    if (NODE_ENV == 'DEV') {
-      let loginInfo = "yh_username=css&yh_password=123456"
-      let headers = { "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8", }
-      postApi('/api/user/login')('', loginInfo, {headers} ).then((res) => {
-        if (res.code == 'S200') { console.log('login success') }
-      })
-      return 
-    }
-  }, [])
-
 	return (
 		<Router>
 			<Switch>
-				
-				<Route exact path="/middleware" render={() => <Redirect to="/middleware/mysql" push />} />
-				
+				<Route
+					exact
+					path="/middleware"
+					render={() => <Redirect to="/middleware/mysql" push />}
+				/>
+
 				{/* Page */}
 				<PageIndex {...props} />
 
