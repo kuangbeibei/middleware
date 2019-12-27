6ec25b9759e13111ba785a4a80b3273f8dc4e420
kup <332518418@qq.com>
2019-12-27 15:42:00 +0800

    adjust

--- src/pages/Redis/Home/index.tsx
+++ src/pages/Redis/Home/index.tsx
@@ -245,7 +245,7 @@ function RedisCluster(props) {
 				}
 			})
 			.catch(e => {
-				console.log('拓扑, ', e, '..', e.response && e.response.status);
+				console.log('拓扑, ', e, '..', e.response && e.response.status, 'e.message,', e.message);
 				if (e.response && e.response.status && e.response.status === 500) {
 					message.error('集群状态异常，不能获取')
 				} else {
