8ef0b9b14ac64f2c08efb029bd8b18f6323f6aef
kup <332518418@qq.com>
2020-01-02 18:19:40 +0800

    mysql备份策略

--- src/pages/Mysql/Detail/Backup-tab.tsx
+++ src/pages/Mysql/Detail/Backup-tab.tsx
@@ -9,22 +9,34 @@ import { useState, useEffect } from "react";
 import { Table, Card, Switch, Icon, InputNumber } from "antd";
 import { FormatTime } from "@utils/tools";
 
-export default function(props) {
+export default function (props) {
 	const {
 		backList,
 		basicData: { backupStrategy, backupKeepDays }
 	} = props;
 
-	console.log(backList, backupStrategy, backupKeepDays);
-
 	const [tableList, settableList] = useState(Array());
 	const [isBackupstrategyEditing, setisBackupstrategyEditing] = useState(false);
-	const [isBackcupDaysEditing, setisBackcupDaysEditing] = useState(false)
+	const [isBackcupDaysEditing, setisBackcupDaysEditing] = useState(false);
+
+	const [hour, sethour] = useState(0);
+	const [minute, setminute] = useState(0);
+	const [days, setdays] = useState(0);
+	const [isStrategyRunning, setisStrategyRunning] = useState(true);
+	const [isDaysRunning, setisDaysRunning] = useState(true)
 
 	useEffect(() => {
 		if (Array.isArray(backList)) {
 			settableList(backList);
 		}
+		backupStrategy
+			.replace(/\*/g, "")
+			.split(" ")
+			.filter(i => i)
+			.forEach((item, idx) => {
+				idx === 0 ? setminute(item) : sethour(item);
+			});
+		setdays(backupKeepDays);
 	}, []);
 
 	const columns = [
@@ -58,69 +70,120 @@ export default function(props) {
 		return (
 			<>
 				<span className="stop">{`停`}</span>
-				<Switch />
+				<Switch checked={idx === 1 ? isStrategyRunning : isDaysRunning} />
 				<span className="start">{`启`}</span>
 			</>
 		);
 	};
 
+	/**
+	 * 编辑备份信息
+	 * @param type
+	 */
 	const editContent = type => {
-		if (type === 'strategy') {
-
+		if (type === "strategy") {
+			setisBackupstrategyEditing(!isBackupstrategyEditing);
 		} else {
-			
+			setisBackcupDaysEditing(!isBackcupDaysEditing);
 		}
-	}
+	};
 
 	const actionChange = (status, type) =>
-		status
-			? [<Icon type="edit" theme="twoTone" onClick={() => editContent(type)} />]
+		!status
+			? [
+				<Icon
+					type="edit"
+					theme="twoTone"
+					onClick={() => editContent(type)}
+				/>
+			]
 			: [
-					<Icon type="close-square" theme="twoTone" />,
-					<Icon type="check-square" theme="twoTone" />
-			  ];
+				<Icon
+					type="close-square"
+					theme="twoTone"
+					onClick={() => editContent(type)}
+				/>,
+				<Icon
+					type="check-square"
+					theme="twoTone"
+					onClick={() => editContent(type)}
+				/>
+			];
 
 	return (
 		<>
-			<Card
-				title={"备份清理策略"}
-				extra={switchBtn(2)}
-				actions={actionChange("isBackupstrategyEditing", 'strategy')}
-			>
-				{
-					<>
-						<span style={{display: isBackupstrategyEditing ? 'none' : 'block'}}>{backupStrategy}</span>
-						<div style={{display: isBackupstrategyEditing ? 'block' : 'none'}}>
-							<InputNumber
-								// defaultValue={hour}
-								min={0}
-								max={6}
-								formatter={value => `${value}时`}
+			<div style={{ display: "flex", marginBottom: "30px" }}>
+				<Card
+					title={"备份清理策略"}
+					extra={switchBtn(2)}
+					actions={actionChange(isBackupstrategyEditing, "strategy")}
+				>
+					{
+						<>
+							<span
+								style={{
+									display: isBackupstrategyEditing
+										? "none"
+										: "block"
+								}}
+							>
+								{backupStrategy}
+							</span>
+							<div
+								style={{
+									display: isBackupstrategyEditing
+										? "block"
+										: "none"
+								}}
+							>
+								<InputNumber
+									value={hour}
+									min={0}
+									max={6}
+									formatter={value => `${value}时`}
 								// onChange={val => setBackupTime("hour", val)}
-							/>
-							<InputNumber
-								// defaultValue={minute}
-								min={0}
-								max={59}
-								formatter={value => `${value}分`}
+								/>
+								<InputNumber
+									value={minute}
+									min={0}
+									max={59}
+									formatter={value => `${value}分`}
 								// onChange={val => setBackupTime("minute", val)}
+								/>
+							</div>
+						</>
+					}
+				</Card>
+				<Card
+					title={"备份时间"}
+					extra={switchBtn(1)}
+					actions={actionChange(isBackcupDaysEditing, "days")}
+				>
+					{
+						<>
+							<span
+								style={{
+									display: isBackcupDaysEditing
+										? "none"
+										: "block"
+								}}
+							>
+								{backupKeepDays}
+							</span>
+							<InputNumber
+								value={days}
+								min={-1}
+								formatter={value => `${value}天`}
+								style={{
+									display: isBackcupDaysEditing
+										? "block"
+										: "none"
+								}}
 							/>
-						</div>
-					</>
-				}
-			</Card>
-			<Card
-				title={"备份时间"}
-				extra={switchBtn(1)}
-				actions={actionChange("isBackcupDaysEditing", 'days')}
-			>
-				{
-					<>
-						<span style={{display: isBackcupDaysEditing ? 'none' : 'block'}}>{backupKeepDays}</span>
-						<InputNumber min={-1} formatter={value => `${value}天`} style={{display: isBackcupDaysEditing ? 'block' : 'none'}}/>
-					</>
-				}
-			</Card>
+						</>
+					}
+				</Card>
+			</div>
 
 			<Table
 				columns={columns}
