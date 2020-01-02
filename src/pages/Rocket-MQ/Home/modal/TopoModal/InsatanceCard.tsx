6502d0cb33629fef2597f961c1a0bb790e0f64a7
kevinkang <313751672@qq.com>
2020-01-02 18:41:45 +0800

    rmq topo modify

---
+++ src/pages/Rocket-MQ/Home/modal/TopoModal/InsatanceCard.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Icon } from 'antd';
+
+function InstanceCard(props) {
+  
+  return (
+    <>
+      <div>
+        <Icon type="database" />
+        
+      </div>
+    </>
+  )
+}
+
+export default InstanceCard;
