82e026a3e78e6f2a0219a176affea1ae55936f17
kup <332518418@qq.com>
2020-01-03 15:31:03 +0800

    log common

---
+++ src/Funcs/Log-process.tsx
@@ -0,0 +1,44 @@
+/*
+ * 颜色处理output
+ * @Date: 2020-01-03 15:26:03
+ * @Last Modified: 2020-01-03 15:26:03
+ */
+
+import * as React from "react";
+
+import { YhText } from "@styled/Text";
+
+/**
+ * @param output
+ */
+export const processLog = loginfo => {
+	let _output;
+	if (loginfo) {
+		_output = loginfo.split(/\n/g);
+		let resOutput = _output.reduce((prev, cur) => {
+			return (
+				<>
+					{prev}
+					<YhText
+						type={
+							cur.startsWith("ok")
+								? "success"
+								: cur.startsWith("changed")
+								? "warning"
+								: cur.startsWith("warning")
+								? "warning"
+								: ""
+						}
+					>
+						{cur}
+					</YhText>
+					<br />
+				</>
+			);
+		}, "");
+		return <>{resOutput}</>;
+	} else {
+		return null;
+	}
+};
+
---
+++ src/components/Terminal-log.modal.tsx
@@ -0,0 +1,75 @@
+/*
+ * 弹窗展示多个历史部署日志
+ * @Date: 2020-01-03 14:50:50
+ * @Last Modified: 2020-01-03 14:50:50
+ */
+
+import * as React from "react";
+
+import { Tabs } from "antd";
+
+import { useEffect } from "react";
+
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import setTableModalVisibility from "@actions/setModalVisibility";
+
+import Modal from "@com/Modal";
+
+import { YhTerminal } from "@styled/Terminal";
+import { FormatTime } from "@utils/tools";
+import { processLog } from "@funcs/Log-process";
+
+function LogsModal(props) {
+	const { tableModalVisibility, setTableModalVisibility, logs } = props;
+
+	useEffect(() => {
+		setTableModalVisibility();
+	}, []);
+
+	const handleCancel = () => {
+		setTableModalVisibility();
+	};
+
+	return (
+		<Modal
+			modalName={`集群部署日志`}
+			visible={tableModalVisibility.visible}
+			handleOk={handleCancel}
+			handleCancel={handleCancel}
+			width={1200}
+		>
+			<Tabs
+				defaultActiveKey="0"
+				tabPosition={"left"}
+				style={{ height: 520 }}
+			>
+				{Array.isArray(logs) &&
+					logs.map((log, i) => (
+						<Tabs.TabPane
+							tab={`${FormatTime(log.ctime)}`}
+							key={`${i}`}
+						>
+							<YhTerminal width={1200}>
+								<div>
+									<pre>{processLog(log.msg)}</pre>
+								</div>
+							</YhTerminal>
+						</Tabs.TabPane>
+					))}
+			</Tabs>
+		</Modal>
+	);
+}
+
+export default connect(
+	(state: any) => ({
+		tableModalVisibility: state.tableModalVisibility
+	}),
+	dispatch => ({
+		setTableModalVisibility: bindActionCreators(
+			setTableModalVisibility,
+			dispatch
+		)
+	})
+)(LogsModal);
