34cdfcb927d364837bf2c181431e363a19642d64
kevinkang <313751672@qq.com>
2020-01-03 14:09:03 +0800

    add loading on basicinfo of rmq

--- src/pages/Rocket-MQ/Detail/Tab-Content/BasicInfo.tsx
+++ src/pages/Rocket-MQ/Detail/Tab-Content/BasicInfo.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { Descriptions } from "antd";
 import PasswordColumn from "@com/Password-unit";
+import Loading from "@com/UI/Loading";
 
 
 
@@ -10,7 +11,7 @@ function BasicInfo (props) {
 
   let { match: { params: { id: clusterId } } } = props
   let [clusterConfig, setClusterConfig] = useState([])
-
+  let [loading, setLoading] = useState(true)
 
   const processPass = pass => {
 		return <PasswordColumn pass={pass} />;
@@ -21,7 +22,7 @@ function BasicInfo (props) {
 		return val.reduce(
 			(prev, cur, idx) => {
         let curInfo = (
-          <div key={cur.ip}>
+          <div key={cur.ip + idx}>
             <span>{cur.ip}:{cur.port}    用户名: {cur.user} </span>   密码: <PasswordColumn pass={cur.pass} />
           </div>
         )
@@ -34,47 +35,42 @@ function BasicInfo (props) {
 
   useEffect(()=>{
     getClusterConfigInfo(clusterId).then(data => {
-      console.log(data, 'data')
       setClusterConfig(data)
+      setLoading(false)
     })
   }, [clusterId])
   
 
+  const Items = clusterConfig.map((item:any) => {
+    let val
+    if (typeof item.value === 'string') {
+      val = item.value.replace(/\n/g, "\n");
+    }
+    if (Array.isArray(item.value)) {
+      val = processInstancesData(item.value)
+    }
+
+    return (
+      <Descriptions.Item label={item.name} key={item.value}>
+        {
+          Array.isArray(item.value) ? <div className="instances-td" > {val} </div> : <pre> {val || '--'} </pre>
+        }
+      </Descriptions.Item>
+    )
+
+  })
+
   return (
-    <Descriptions  column={1}  bordered>
-
-
+    <>
       {
-
-        clusterConfig.map((item:any) => {
-          let val
-          if (typeof item.value === 'string') {
-						val = item.value.replace(/\n/g, "\n");
-          }
-          if (Array.isArray(item.value)) {
-            val = processInstancesData(item.value)
-          }
-
-          return (
-            <Descriptions.Item label={item.name} key={item.value}>
-
-              {
-                Array.isArray(item.value) ? <div className="instances-td" > {val} </div> : <pre> {val || '--'} </pre>
-
-              }
-
-            </Descriptions.Item>
-          )
-
-        })
-
-
+        loading ? <Loading/> :
+        (
+          <Descriptions  column={1}  bordered>
+            {Items}
+          </Descriptions>
+        )
       }
-
-
-
-
-  </Descriptions>
+     </>
   )
 }
 
