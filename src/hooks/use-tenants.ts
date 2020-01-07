a77b172b4b78b2e35add6e650b1a97159276642c
kup <332518418@qq.com>
2020-01-07 18:19:52 +0800

    format code

--- src/hooks/use-tenants.ts
+++ src/hooks/use-tenants.ts
@@ -5,22 +5,20 @@ import { ProductApiUrl } from "@utils/data";
 const { ProductUumApiUrl } = ProductApiUrl;
 
 // 把接口调用单独提出来，只调用一次即可
-const getTenantList = (
-	async () => {
+const getTenantList = (async () => {
 	let res = await getApi(ProductUumApiUrl)(`/tenant/os/list`)
-			.then(res => {
-				try {
-					if (res.data && Array.isArray(res.data)) {
-						return res.data
-					}
-				} catch (e) {
-					return res;
+		.then(res => {
+			try {
+				if (res.data && Array.isArray(res.data)) {
+					return res.data;
 				}
-			})
+			} catch (e) {
+				return res;
+			}
+		})
 		.catch(e => Promise.reject(e));
 	return res;
-}
-)()
+})();
 
 function useTenants() {
 	let [tenants, setTenants] = useState<Array<any>>([]);
@@ -29,15 +27,14 @@ function useTenants() {
 		getTenantList.then(list => {
 			// console.log('list,', list);
 			setTenants(list);
-		})
+		});
 	}, []);
 	// console.log('memo list,', tenants);
 	// console.log('getlist,', getlist);
 
-	useEffect(() => getlist, [])
+	useEffect(() => getlist, []);
 
-	return tenants
+	return tenants;
 }
-
 
 export default useTenants;
